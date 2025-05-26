b7abe94944eb5a17125fa060003245f841a761f2
Nico Schulze <nico.schulze1602@gmail.com>
2025-05-26 19:49:03 +0200

    define constants at the top of the module

--- data_fetcher.py
+++ data_fetcher.py
@@ -3,16 +3,16 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+API_KEY = os.getenv("API_KEY")
 
 def fetch_data(animal_name):
     """Fetches animal data from the API."""
-    api_key = os.getenv("API_KEY")
-    if not api_key:
+    if not API_KEY:
         print("❌ Error: API key not found. Please check your .env file.")
         return None
 
     api_url = f'https://api.api-ninjas.com/v1/animals?name={animal_name}'
-    response = requests.get(api_url, headers={'X-Api-Key': api_key})
+    response = requests.get(api_url, headers={'X-Api-Key': API_KEY})
 
     if response.status_code == requests.codes.ok:
         return response.json()
